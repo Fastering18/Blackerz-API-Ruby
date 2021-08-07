225241f9d39490b757ed02ff316e57ff48610a3e
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-08-07 22:41:02 +0700

    First create

---
+++ lib/blackerz/sdk.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+#require "http"
+require_relative "sdk/version"
+
+module Blackerz
+  #module Sdk
+  #  class Error < StandardError; end
+  # Your code goes here...
+  #end
+  
+  class Bots
+  def self.bot_data(bot_id)
+    res = HTTP.get("https://blackerz.herokuapp.com/api/v1/bots/" + bot_id)
+     unless res.code == 200 then 
+       nil
+     end
+     data = res.parse
+     data
+  end
+end
+end
